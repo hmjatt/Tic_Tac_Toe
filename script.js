97ef6f077e5a5008759bc4b5625be496d442f872
Harmeet Matharoo <hmjatt@gmail.com>
2022-03-20 21:25:06 -0600

    try 3

--- script.js
+++ script.js
@@ -1,230 +1,92 @@
-//script for Tic Tac Toe
-
-
-
-
-
-const gameController = (() => {
-
-    const statusDisplay = document.getElementById('game-status');
-
-    
-
-    let currentPlayer = "X";
-
-    let gameActive = true;
-    
-
-    let gameState = ["", "", "", "", "", "", "", "", ""];
-
-
-
-
-    const winningMessage = () => `Player ${currentPlayer} has won!`;
-    const drawMessage = () => `Game ended in a draw!`;
-    const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
-
-    statusDisplay.innerHTML = currentPlayerTurn();
-
-
-    // works
-    const clickedCell = (clickedCellEvent) =>{
-        let clicked = clickedCellEvent.target;
-        let clickedAttr = (parseInt(clicked.getAttribute('data-cell-index')));
-        if (gameState[clickedAttr] == "" && gameActive == true) {
-            
-            gameState[clickedAttr] = currentPlayer;
-            clicked.innerHTML = currentPlayer;
-            // verify if player won
-            handleResultValidation();
-            
-            
-        } else if (gameState[clickedAttr] !== "" || !gameActive) {
-           
-            return;
-        }
-
-
-    };
-
-        
-
-    const winningConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-
-
-
-    const handleResultValidation = () => {
-        let roundWon = false;
-        for (let i = 0; i <= 7; i++) {
-            const winCondition = winningConditions[i];
-            let a = gameState[winCondition[0]];
-            let b = gameState[winCondition[1]];
-            let c = gameState[winCondition[2]];
-            if (a === '' || b === '' || c === '') {
-                continue;
-            }
-            if (a === b && b === c) {
-                roundWon = true;
-                break
-            }
-        }
-        if (roundWon) {
-                statusDisplay.innerHTML = winningMessage();
-                gameActive = false;
-                return;
-            }
-        /* 
-        We will check weather there are any values in our game state array 
-        that are still not populated with a player sign
-        */
-            let roundDraw = !gameState.includes("");
-            if (roundDraw) {
-                statusDisplay.innerHTML = drawMessage();
-                gameActive = false;
-                return;
-            }
-        /*
-        If we get to here we know that the no one won the game yet, 
-        and that there are still moves to be played, so we continue by changing the current player.
-        */
-            handlePlayerChange();
-            function handlePlayerChange() {
-                currentPlayer = currentPlayer === "X" ? "O" : "X";
-
-                if(currentPlayer == "O") {
-                    console.log("its ai turn");
-                    //if its ai turn make ai decision based apon its difficulty
-                    // /*
-                    //  * Constructs an AI player with a specific level of intelligence
-                    //  * @param level [String]: the desired level of intelligence
-                    //  */
-                    // let ai = function(level) {
-
-                    //     //private attribute: level of intelligence the player has
-                    //     var levelOfIntelligence = level;
-
-                    //     //private attribute: the game the player is playing
-                    //     var game = {};
-
-                    //     /*
-                    //      * private recursive function that computes the minimax value of a game state
-                    //      * @param state [State] : the state to calculate its minimax value
-                    //      * @returns [Number]: the minimax value of the state
-                    //      */
-                    //     function minimaxValue(state) { ... }
-
-                    //     /*
-                    //      * private function: make the ai player take a blind move
-                    //      * that is: choose the cell to place its symbol randomly
-                    //      * @param turn [String]: the player to play, either X or O
-                    //      */
-                    //     function takeABlindMove(turn) { ... }
-
-                    //     /*
-                    //      * private function: make the ai player take a novice move,
-                    //      * that is: mix between choosing the optimal and suboptimal minimax decisions
-                    //      * @param turn [String]: the player to play, either X or O
-                    //      */
-                    //     function takeANoviceMove(turn) { ... }
-
-                    //     /*
-                    //      * private function: make the ai player take a master move,
-                    //      * that is: choose the optimal minimax decision
-                    //      * @param turn [String]: the player to play, either X or O
-                    //      */
-                    //     function takeAMasterMove(turn) { ... }
-
-
-                    //     /*
-                    //      * public method to specify the game the ai player will play
-                    //      * @param _game [Game] : the game the ai will play
-                    //      */
-                    //     this.plays = function(_game){
-                    //         game = _game;
-                    //     };
-
-                    //     /*
-                    //      * public function: notify the ai player that it's its turn
-                    //      * @param turn [String]: the player to play, either X or O
-                    //      */
-                    //     this.notify = function(turn) {
-                    //         switch(levelOfIntelligence) {
-                    //             //invoke the desired behavior based on the level chosen
-                    //             case "blind": takeABlindMove(turn); break;
-                    //             case "novice": takeANoviceMove(turn); break;
-                    //             case "master": takeAMasterMove(turn); break;
-                    //         }
-                    //     };
-                    // };
-
-                }else if(currentPlayer == "X") {
-                    console.log("its player turn");
-                }
-
-                statusDisplay.innerHTML = currentPlayerTurn();
-            }
-        
-
-        
-    
-    };
-
-    
-
-
-        
-
-    
-        
-    
-    return {
-        clickedCell,
-        
-    };
-
-
-})();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-And finally we add our event listeners to the actual game cells, as well as our 
-restart button
-*/
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', (currentCell) => {
-    gameController.clickedCell(currentCell);
-        
-        
-    }));
-// document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
-
-
-
+var origBoard;
+const huPlayer = 'O';
+const aiPlayer = 'X';
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
+
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {
+	document.querySelector(".endgame").style.display = "none";
+	origBoard = Array.from(Array(9).keys());
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+	}
+}
+
+function turnClick(square) {
+	if (typeof origBoard[square.target.id] == 'number') {
+		turn(square.target.id, huPlayer)
+		if (!checkTie()) turn(bestSpot(), aiPlayer);
+	}
+}
+
+function turn(squareId, player) {
+	origBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+	let gameWon = checkWin(origBoard, player)
+	if (gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) => 
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function gameOver(gameWon) {
+	for (let index of winCombos[gameWon.index]) {
+		document.getElementById(index).style.backgroundColor =
+			gameWon.player == huPlayer ? "blue" : "red";
+	}
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+	declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
+}
+
+function declareWinner(who) {
+	document.querySelector(".endgame").style.display = "block";
+	document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+	return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+	return emptySquares()[0];
+}
+
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "green";
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("Tie Game!")
+		return true;
+	}
+	return false;
+}
 
 
 
