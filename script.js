81de634da6a34b629d98d56d40ce35e783e56638
Harmeet Matharoo <hmjatt@gmail.com>
2022-03-21 19:26:12 -0600

    stage 4/7 complete, its modular

--- script.js
+++ script.js
@@ -1,106 +1,102 @@
-var origBoard;
-const huPlayer = 'O';
-const aiPlayer = 'X';
-const winCombos = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[6, 4, 2]
-]
 
-const cells = document.querySelectorAll('.cell');
-startGame();
 
-function startGame() {
-	document.querySelector(".endgame").style.display = "none";
-	origBoard = Array.from(Array(9).keys());
-	for (var i = 0; i < cells.length; i++) {
-		cells[i].innerText = '';
-		cells[i].style.removeProperty('background-color');
-		cells[i].addEventListener('click', turnClick, false);
+//create gameBoard using factory fxn & concatenative Inheritance/cloning
+const gameBoard = {
+
+	
+	origBoard: function origBoard() {
+		return this.board;
 	}
-}
+	
+};
 
-function turnClick(square) {
-	if (typeof origBoard[square.target.id] == 'number') {
-		turn(square.target.id, huPlayer)
-		if (!checkTie()) turn(bestSpot(), aiPlayer);
+
+// create players using factory fxn & concatenative Inheritance/cloning
+const players = {
+
+	huPlayer: function huPlayer() {
+		return this.human;
+	},
+
+	aiPlayer: function aiPlayer() {
+		return this.robot;
 	}
+
 }
 
-function turn(squareId, player) {
-	origBoard[squareId] = player;
-	document.getElementById(squareId).innerText = player;
-	let gameWon = checkWin(origBoard, player)
-	if (gameWon) gameOver(gameWon)
-}
 
-function checkWin(board, player) {
-	let plays = board.reduce((a, e, i) => 
-		(e === player) ? a.concat(i) : a, []);
-	let gameWon = null;
-	for (let [index, win] of winCombos.entries()) {
-		if (win.every(elem => plays.indexOf(elem) > -1)) {
-			gameWon = {index: index, player: player};
-			break;
-		}
-	}
-	return gameWon;
-}
 
-function gameOver(gameWon) {
-	for (let index of winCombos[gameWon.index]) {
-		document.getElementById(index).style.backgroundColor =
-			gameWon.player == huPlayer ? "blue" : "red";
-	}
-	for (var i = 0; i < cells.length; i++) {
-		cells[i].removeEventListener('click', turnClick, false);
-	}
-	declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
-}
 
-function declareWinner(who) {
-	document.querySelector(".endgame").style.display = "block";
-	document.querySelector(".endgame .text").innerText = who;
-}
+const gameController = (() => {
 
-function emptySquares() {
-	return origBoard.filter(s => typeof s == 'number');
-}
+	const winCombos = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[6, 4, 2]
+	]
 
-function bestSpot() {
-	return emptySquares()[0];
-}
 
-function checkTie() {
-	if (emptySquares().length == 0) {
+	return {
+		winCombos,
+	};
+})();
+
+
+
+
+const ticTacToe = (() => {
+	
+	//make a reference to gameBoard using factory fxn & concatenative Inheritance/cloning
+	const getBoard = Object.assign({}, gameBoard, {board: ''});
+	
+	const cells = document.querySelectorAll('.cell');
+
+	//make a reference to human player
+	let human = Object.assign({}, players, {human: 'O'});
+	let huPlayer = human.huPlayer();
+
+	//make a reference to ai player
+	let robot = Object.assign({}, players, {robot: 'X'});
+
+	const startGame = () => {
+		document.querySelector(".endgame").style.display = "none";
+
+		theBoard = getBoard.origBoard();
+
+		origBoard = Array.from(Array(9).keys());
 		for (var i = 0; i < cells.length; i++) {
-			cells[i].style.backgroundColor = "green";
-			cells[i].removeEventListener('click', turnClick, false);
+			cells[i].innerText = '';
+			cells[i].style.removeProperty('background-color');
+			cells[i].addEventListener('click', turnClick, false);
 		}
-		declareWinner("Tie Game!")
-		return true;
 	}
-	return false;
-}
+
+	const turnClick =(square) => {
+		turn(square.target.id, huPlayer);
+	}
+
+	const turn = (squareId, player) => {
+		origBoard[squareId] = player;
+		document.getElementById(squareId).innerText = player;
+		console.log("it works")
+	}
 
 
 
 
+	
+	return {
+	  startGame,
+	  turnClick,
+	  turn,
+	  
+	};
+  })();
+  
 
-
-
-
-
-
-
-
-
-
-
-
-
+  document.onload = ticTacToe.startGame();
