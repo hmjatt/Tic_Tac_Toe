fd11f00178d570a2869516e5eb27f8f36ce94ede
Harmeet Matharoo <hmjatt@gmail.com>
2022-03-22 23:38:12 -0600

    stage 7/7 ai is unbeatable

--- script.js
+++ script.js
@@ -105,8 +105,64 @@ const ticTacToe = (() => {
 	}
 	
 	const bestSpot= () => {
-		return emptySquares()[0];
+		return minimax(origBoard, aiPlayer).index;
 	}
+
+
+
+	const minimax = (newBoard, player) => {
+		var availSpots = emptySquares();
+	
+		if (resultValidation.checkWin(newBoard, huPlayer)) {
+			return {score: -10};
+		} else if (resultValidation.checkWin(newBoard, aiPlayer)) {
+			return {score: 10};
+		} else if (availSpots.length === 0) {
+			return {score: 0};
+		}
+		var moves = [];
+		for (var i = 0; i < availSpots.length; i++) {
+			var move = {};
+			move.index = newBoard[availSpots[i]];
+			newBoard[availSpots[i]] = player;
+	
+			if (player == aiPlayer) {
+				var result = minimax(newBoard, huPlayer);
+				move.score = result.score;
+			} else {
+				var result = minimax(newBoard, aiPlayer);
+				move.score = result.score;
+			}
+	
+			newBoard[availSpots[i]] = move.index;
+	
+			moves.push(move);
+		}
+	
+		var bestMove;
+		if(player === aiPlayer) {
+			var bestScore = -10000;
+			for(var i = 0; i < moves.length; i++) {
+				if (moves[i].score > bestScore) {
+					bestScore = moves[i].score;
+					bestMove = i;
+				}
+			}
+		} else {
+			var bestScore = 10000;
+			for(var i = 0; i < moves.length; i++) {
+				if (moves[i].score < bestScore) {
+					bestScore = moves[i].score;
+					bestMove = i;
+				}
+			}
+		}
+	
+		return moves[bestMove];
+	}
+
+
+
 
 	
 	return {
