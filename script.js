08509bc7587a41f040ed9cc5e5312a0ec6b3198c
Harmeet Matharoo <hmjatt@gmail.com>
2022-03-22 21:51:52 -0600

    stage 6/7 works, ai takes next empty spot

--- script.js
+++ script.js
@@ -52,8 +52,10 @@ const gameController = (() => {
 const ticTacToe = (() => {
 	
 	//make a reference to gameBoard using factory fxn & concatenative Inheritance/cloning
-	const getBoard = Object.assign({}, gameBoard, {board: ''});
+	const getBoard = Object.assign({}, gameBoard, {board: [0, 1, 2, 3, 4, 5, 6, 7, 8]});
 	let origBoard = getBoard.origBoard();
+
+	// console.log(typeof(origBoard), origBoard);
 	
 	const cells = document.querySelectorAll('.cell');
 
@@ -63,6 +65,7 @@ const ticTacToe = (() => {
 
 	//make a reference to ai player
 	let robot = Object.assign({}, players, {robot: 'X'});
+	let aiPlayer = robot.aiPlayer();
 
 	const startGame = () => {
 		document.querySelector(".endgame").style.display = "none";
@@ -75,10 +78,17 @@ const ticTacToe = (() => {
 			cells[i].style.removeProperty('background-color');
 			cells[i].addEventListener('click', turnClick, false);
 		}
+		console.log(typeof(origBoard), origBoard);
+
 	}
 
 	const turnClick =(square) => {
-		turn(square.target.id, huPlayer);
+		// turn(square.target.id, huPlayer);
+
+		if (typeof origBoard[square.target.id] == 'number') {
+			turn(square.target.id, huPlayer)
+			if (!resultValidation.checkWin(origBoard, huPlayer) && !resultValidation.checkTie()) turn(bestSpot(), aiPlayer);
+		}
 	}
 
 	const turn = (squareId, player) => {
@@ -88,11 +98,22 @@ const ticTacToe = (() => {
 		if (gameWon) resultValidation.gameOver(gameWon);
 	}
 
+
+	const emptySquares = () => {
+		let x = origBoard.filter(s => typeof s == 'number');
+		return x;
+	}
+	
+	const bestSpot= () => {
+		return emptySquares()[0];
+	}
+
 	
 	return {
 	  startGame,
 	  turnClick,
 	  cells,
+	  emptySquares,
 	};
   })();
 
@@ -106,6 +127,7 @@ const resultValidation = (() => {
 	let huPlayer = human.huPlayer();
 
 	let winCombos = gameController.winCombos;
+	// console.log(typeof(winCombos), winCombos);
 	let cells = ticTacToe.cells;
 
 	const checkWin = (board, player) => {
@@ -129,14 +151,35 @@ const resultValidation = (() => {
 		for (var i = 0; i < cells.length; i++) {
 			cells[i].removeEventListener('click', ticTacToe.turnClick, false);
 		}
+		declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
+	}
+
+
+	const checkTie = () => {
+		if (ticTacToe.emptySquares().length == 0) {
+			for (var i = 0; i < cells.length; i++) {
+				cells[i].style.backgroundColor = "green";
+				cells[i].removeEventListener('click', ticTacToe.turnClick, false);
+			}
+			declareWinner("Tie Game!")
+			return true;
+		}
+		return false;
+	}
+
+	const declareWinner = (who) => {
+		document.querySelector(".endgame").style.display = "block";
+		document.querySelector(".endgame .text").innerText = who;
 	}
 
 	return {
 		checkWin,
 		gameOver,
-		// gameWon,
+		checkTie,
 	};
 })();
+
+
 
 
   
